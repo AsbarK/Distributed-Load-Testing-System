db7c51223ef78b666f517a5ceda05a4221bb23ef
asbar <asbar03KK@gmail.com>
2023-11-20 19:30:47 +0530

    resolved the conflicts

--- driverNode.py
+++ driverNode.py
@@ -34,7 +34,7 @@ async def send_requests_with_delay(url, num_requests, delay_interval_seconds, te
 
         print(f"Request {i + 1} Status Code: {response.status_code}, Response Time: {response_time:.2f} seconds")
         if test_type=='TSUNAMI' and  delay_interval_seconds:
-            await asyncio.sleep(delay_interval_seconds)
+            time.sleep(delay_interval_seconds)
         if sum(response_times)>=0.75:
             mean_response_time = statistics.mean(response_times)
             median_response_time = statistics.median(response_times)
@@ -58,65 +58,55 @@ async def send_requests_with_delay(url, num_requests, delay_interval_seconds, te
 async def process_message(message):
     try:
         if message and message.value:
-            if message.value.decode('utf-8') == 'EOFBREAK':
-                consumer_Test_Conf.close()
             ms = json.loads(message.value.decode('utf-8'))
-            print('test_Id:'+ms['test_id'])
+            print(ms['test_id'])
             await send_requests_with_delay('https://www.google.com', int(ms['message_count_per_driver']), int(ms['test_message_delay']), ms['test_id'],ms['test_type'])
+            producer.flush()
+            # producer.send('metrics',b'EOFBREAK')
+            return
         else:
             print("Received message with None value.")
     except Exception as e:
         print(f"Error processing message: {e}")
 
-# async def consumer_triggerr(consumer_trigger):
-#     global isTrigger
-#     for msgs in consumer_trigger:
-#         print("in trigger")
-#         msg_val = msgs.value.decode('utf-8')
-#         if msg_val == 'EOFBREAK':
-#             consumer_trigger.close()
-#             break
-#         msg_value = json.loads(msg_val)
-#         if msg_value["trigger"] == "YES":
-#             print('yes')
-#             isTrigger = True
-#             print(f"Received message: {msg_value}")
-#             return
+async def consumer_triggerr(consumer_trigger):
+    global isTrigger
+    for msgs in consumer_trigger:
+        print("in trigger")
+        msg_val = msgs.value.decode('utf-8')
+        if msg_val == 'EOFBREAK':
+            consumer_trigger.close()
+            break
+        msg_value = json.loads(msg_val)
+        if msg_value["trigger"] == "YES":
+            print('yes')
+            isTrigger = True
+            print(f"Received message: {msg_value}")
+            return
 
 
 async def consume_messages(consumer_Test_Conf):
-    global isTrigger
     try:
-        # print('p')
+        print('p')
         for message in consumer_Test_Conf:
             print('pout')
-            for msgs in consumer_trigger:
-                print("in trigger")
-                msg_val = msgs.value.decode('utf-8')
-                if msg_val == 'EOFBREAK':
-                    consumer_trigger.close()
-                    break
-                msg_value = json.loads(msg_val)
-                if msg_value["trigger"] == "YES":
-                    # print('yes')
-                    isTrigger = True
-                    # print(f"Received message: {msg_value}")
-            if isTrigger:
-                await process_message(message)
+            await process_message(message)
+            consumer_Test_Conf.close()
+            print('sending break')
+            producer.send('metrics',b'EOFBREAK')
+            return
     except Exception as e:
         print(f"Error consuming messages: {e}")
 
 
 async def main():
-    print('node_Id:'+unique_hash)
+    print(unique_hash)
     producer.send('register', json.dumps({'node_id': unique_hash, 'node_IP': 'IPAddr', 'message_type': 'DRIVER_NODE_REGISTER'}).encode('utf-8'))
-    # task = asyncio.create_task(consumer_triggerr(consumer_trigger))
-    # task.add_done_callback(await consume_messages(consumer_Test_Conf))
-    await consume_messages(consumer_Test_Conf)
+    task = asyncio.create_task(consumer_triggerr(consumer_trigger))
+    task.add_done_callback(await consume_messages(consumer_Test_Conf))
+    return
 
 if __name__ == '__main__':
     producer = KafkaProducer(bootstrap_servers=[kafkaIp])
     consumer_Test_Conf = KafkaConsumer('test_config', group_id=f'{unique_hash}test', bootstrap_servers=[kafkaIp])
     consumer_trigger = KafkaConsumer('trigger', group_id=f'{unique_hash}trigger', bootstrap_servers=[kafkaIp])
-
-    asyncio.run(main())
--- orchetratorNode.py
+++ orchetratorNode.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
-import time
-from kafka import KafkaConsumer,KafkaProducer
+import sys
+from kafka import KafkaConsumer, KafkaProducer
 import asyncio
 import hashlib
 import random
 from datetime import datetime
 import json
+import time
+import requests
+
+num_drivers, test_type, delay, num_messages = sys.argv[1:]
+
+
 def uniqueId():
     unique_identifier = random.randint(1, 10000)
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f') 
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
     unique_string = f"{timestamp}-{unique_identifier}"
     unique_hash = hashlib.sha1(unique_string.encode()).hexdigest()[:8]
     return unique_hash
+
 
 consumer_Register = KafkaConsumer('register', group_id='Register')
 consumer_metrics = KafkaConsumer('metrics', group_id='metrics')
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 rejisterd_DriverNodes = {}
 metric_result = {}
+temp_metric_result = {}
+
 
 async def process_register_message(message):
     try:
@@ -31,6 +40,8 @@ async def process_register_message(message):
         print(f"Error decoding JSON: {e}")
     except Exception as e:
         print(f"Error processing message: {e}")
+
+
 async def process_metric_message(message):
     try:
         message_value = message.value.decode('utf-8')
@@ -39,10 +50,11 @@ async def process_metric_message(message):
             node_id = msg['node_id']
             test_id = str(msg['test_id'])
             metrics = msg['metrics']
-            if node_id not in metric_result:
-                metric_result[node_id] = {}
-            metric_result[node_id][test_id] = metrics
-            print({'node_id':node_id,'metrics':metrics,'test_id':test_id})
+            if node_id not in temp_metric_result:
+                temp_metric_result[node_id] = {}
+            temp_metric_result[node_id][test_id] = metrics
+            print({'node_id': node_id, 'metrics': metrics, 'test_id': test_id})
+
         else:
             print("Received empty or non-JSON message.")
     except json.JSONDecodeError as e:
@@ -50,50 +62,60 @@ async def process_metric_message(message):
     except Exception as e:
         print(f"Error processing message: {e}")
 
-async def consume_messages_resigter(type_consumer,numberOfDriver,typeOfTopic,noTests):
+
+async def consume_messages_resigter(type_consumer, numberOfDriver, typeOfTopic, noTests):
     inde = 0
-    if(typeOfTopic == 'register'):
+    if typeOfTopic == 'register':
         for message in type_consumer:
             await process_register_message(message)
             inde += 1
             if inde >= int(numberOfDriver):
                 type_consumer.close()
                 break
-    elif(typeOfTopic == 'metrics'):
-        coun = 0
-        print(noTests)
+    elif typeOfTopic == 'metrics':
+        inde = 0
         for message in type_consumer:
+            if "EOFBREAK" in message.value.decode('utf-8'):
+                inde+=1
+                if inde >= (int(numberOfDriver)*noTests):
+                    type_consumer.close()
+                    return
             await process_metric_message(message)
-            # coun += 1
-            # if coun >= (int(noTests)*  int(numberOfDriver)):
-            #     type_consumer.close()
-            #     break
-# async def consume_messages_meteric():
+
+
+async def send_metrics_to_flask():
+    while True:
+        # Send temp_metric_result to Flask every one second
+        await asyncio.sleep(1)
+        if temp_metric_result:
+            flask_app_url = 'http://127.0.0.1:5000/update_metrics'
+            response = requests.post(flask_app_url, json={'metric_result': temp_metric_result})
+            print("Temp metric results sent to Flask app")
+            temp_metric_result.clear()
+
+
 async def main():
     numberOfTests = 0
-    numberOfDriverNodes = input('Enter the number of Drivers:')
-    time.sleep(2*int(numberOfDriverNodes))
-    await consume_messages_resigter(consumer_Register, numberOfDriverNodes, 'register',numberOfTests)
+    # numberOfDriverNodes = input('Enter the number of Drivers:')
+    try:
+        await consume_messages_resigter(consumer_Register, num_drivers, 'register', numberOfTests)
 
-    cmdInput = input("Enter a command (1 to send a TSUNAMi, 2 to send AVALANCHI): ")
-    message_count = input("Enter the number of messages:")
-    test_id = uniqueId()
-    if cmdInput == "1":
-        delayInput = input("Enter the delay:")
-        producer.send('test_config', json.dumps({"test_id":test_id,"test_type": "TSUNAMI","test_message_delay": int(delayInput), "message_count_per_driver": int(message_count)}).encode('utf-8'))
-        numberOfTests+=1
-    if cmdInput == "2":
-        producer.send('test_config', json.dumps({"test_id":test_id,"test_type": "AVALANCHE","test_message_delay": "0", "message_count_per_driver": int(message_count)}).encode('utf-8'))
-        numberOfTests+=1
-    cmdInput = input("Enter a command 3 to send trigger as YES: ")
-    if cmdInput == "3":
-        print('in trigger')
-        producer.send('trigger',json.dumps({"test_id":test_id,"trigger": "YES"}).encode('utf-8'))
-        producer.send('trigger',b'EOFBREAK')
-    # time.sleep(2)
-    await consume_messages_resigter(consumer_metrics, numberOfDriverNodes, 'metrics',numberOfTests)
+        test_id = uniqueId()
+        producer.send('test_config', json.dumps({"test_id": test_id, "test_type": str(test_type),"test_message_delay": delay, "message_count_per_driver": num_messages}).encode('utf-8'))
+        numberOfTests += 1
+        # print('in trigger')
+        producer.send('trigger', json.dumps({"test_id": test_id, "trigger": "YES"}).encode('utf-8'))
+        producer.send('trigger', b'EOFBREAK')
+        # time.sleep(2)
+        await asyncio.gather(
+            consume_messages_resigter(consumer_metrics, num_drivers, 'metrics', numberOfTests),
+            send_metrics_to_flask()
+        )
+    except Exception as e:
+        print(f"Error in main: {e}")
 
 if __name__ == '__main__':
+    time.sleep(2 * int(num_drivers))
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-    print(rejisterd_DriverNodes,metric_result)
+    print(rejisterd_DriverNodes, metric_result)
